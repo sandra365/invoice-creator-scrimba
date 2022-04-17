f4e5aff2bf35726001745ff732d35cd1bfe876d0
sandra365 <postnsanna@gmail.com>
2022-04-17 11:40:45 +0300

    finished app

--- index.js
+++ index.js
@@ -1,70 +1,91 @@
-let renderList = [];//check wether all variable types used correctly
-const chosenService = document.getElementById("service-choice");
+let renderList = [];
+const selectedServiceList = document.getElementById("selected-service-list");
 const washCarBtn = document.getElementById("wash-car-btn");
 const mowLawnBtn = document.getElementById("mow-lawn-btn");
 const pullWeedsBtn = document.getElementById("pull-weeds-btn");
+const totalAmountDiv = document.getElementById("total-amount");
 const outcomeBtn = document.getElementById("outcome-btn");
 const services = [
     {
+        id: 1,
         name: "Wash Car",
         price: 10
     },
     {
+        id: 2,
         name: "Mow Lawn",
         price: 20
     },
     {
+        id: 3,
         name: "Pull Weeds",
         price: 30
     }];
 
+
 washCarBtn.addEventListener("click", function() {
-    update(services[0]);
+    add(services[0]);
 })
 
 mowLawnBtn.addEventListener("click", function() {
-    update(services[1]);
+    add(services[1]);
 })
 
 pullWeedsBtn.addEventListener("click", function() {
-    update(services[2]);
+    add(services[2]);
 })
 
 outcomeBtn.addEventListener("click", function() {
-    
+    sendInvoice();
 })
 
 function render (list) { 
-    chosenService.innerHTML = "";
+    selectedServiceList.innerHTML = "";
     list.forEach(listItem => 
         {
             let services = `
             <div class="row">
                 <p class="service-name">
                     ${listItem.name}
+                    <span id="${listItem.id}" class="remove" onclick="removeService(event.target.id)">Remove</span>
                 </p>
                 <p class="service-price">
                     <span class="pale">$</span> ${listItem.price}
                 </p>
             </div>`;
-            chosenService.insertAdjacentHTML('beforeend', services);//check this out on mdn
+            selectedServiceList.insertAdjacentHTML('beforeend', services);//check this out on mdn
          })
 }
 
 function updateTotalAmount() {
     let totalAmount = 0;
-    const totalAmountDIv = document.getElementById("outcome-sum");
     renderList.forEach(service => totalAmount += service.price);
-    totalAmountDIv.innerHTML = `$ ${totalAmount}`;
+    totalAmountDiv.innerHTML = `$ ${totalAmount}`;
 }
 
-function update(service) {
+function add(service) {
     if(!renderList.find(el => el === service)) {
         renderList.push(service);
     }
     render(renderList);
     updateTotalAmount();
 }
+
+function removeService(serviceId) {
+    const index = renderList.findIndex(el => el.id == serviceId);
+    renderList.splice(index, 1);
+    render(renderList);
+    updateTotalAmount();
+}
+
+function sendInvoice() {
+    renderList = [];
+    selectedServiceList.innerHTML = "";
+    totalAmountDiv.innerHTML = `$ 0`
+}
+
+// what on Earth is jQuery?
+
 
 
 
